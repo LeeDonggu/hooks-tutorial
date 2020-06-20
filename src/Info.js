b45b5ceb4b17b4104ab7ed72f14cc7bfc803193d
LEEDONGGU <donggu19940717@gmail.com>
2020-06-20 18:05:34 +0900

    react hooks tutorial success

--- src/Info.js
+++ src/Info.js
@@ -1,20 +1,12 @@
-import React, { useReducer } from "react";
+import React from "react";
+import useInputs from "./useInputs";
 
-function reducer(state, action) {
-  return {
-    ...state,
-    [action.name]: action.value,
-  };
-}
 const Info = () => {
-  const [state, dispatch] = useReducer(reducer, {
+  const [state, onChange] = useInputs({
     name: "",
     nickname: "",
   });
   const { name, nickname } = state;
-  const onChange = (e) => {
-    dispatch(e.target);
-  };
 
   return (
     <div>
